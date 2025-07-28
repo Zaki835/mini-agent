783ee59b98436e160b4f08fd827e8d2596f74909
Mohomed089 <mohomedali709@gmail.com>
2025-07-28 12:13:41 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:mini_agent_browser/screens/browser_screen.dart';
 
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Mini Agent Browser',
+      theme: ThemeData.dark().copyWith(
+        colorScheme: ColorScheme.dark(
+          primary: Colors.blue.shade800,
+          secondary: Colors.blue.shade600,
+          surface: Colors.grey.shade900,
+          background: Colors.grey.shade900,
+        ),
+        scaffoldBackgroundColor: Colors.grey.shade900,
+        appBarTheme: AppBarTheme(
+          backgroundColor: Colors.grey.shade850,
+          elevation: 1,
+        ),
+      ),
+      home: const BrowserScreen(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
