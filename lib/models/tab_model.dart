f51ed6bcd6113737b9bb66de1991cb126d28d86c
Mohomed089 <mohomedali709@gmail.com>
2025-07-28 12:11:07 +0300

    Update tab_model.dart

--- lib/models/tab_model.dart
+++ lib/models/tab_model.dart
@@ -1,1 +1,27 @@
+class TabModel {
+  final String id;
+  String title;
+  String url;
+  bool isLoading;
 
+  TabModel({
+    required this.id,
+    required this.title,
+    required this.url,
+    this.isLoading = false,
+  });
+
+  TabModel copyWith({
+    String? id,
+    String? title,
+    String? url,
+    bool? isLoading,
+  }) {
+    return TabModel(
+      id: id ?? this.id,
+      title: title ?? this.title,
+      url: url ?? this.url,
+      isLoading: isLoading ?? this.isLoading,
+    );
+  }
+}
